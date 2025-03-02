adbc8faab8258ce615cbcc57ba2b527e3887c3af
mspade312 <mspade312@gmail.com>
2025-03-02 11:11:30 -0500

    Avatar Buttons Update

--- SpadesSwiftOutline/Menu Screens/AvatarScreen.swift
+++ SpadesSwiftOutline/Menu Screens/AvatarScreen.swift
@@ -5,4 +5,114 @@
 //  Created by Michael Spade on 10/2/24.
 //
 
-import Foundation
+import UIKit
+
+class AvatarScreen: UITableViewController
+{
+    // MARK: - Properties
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var avatar1: UIButton?
+    @IBOutlet weak var avatar2: UIButton?
+    @IBOutlet weak var avatar3: UIButton?
+    @IBOutlet weak var avatar4: UIButton?
+    @IBOutlet weak var avatar5: UIButton?
+    @IBOutlet weak var avatar6: UIButton?
+    @IBOutlet weak var avatar7: UIButton?
+    @IBOutlet weak var avatar8: UIButton?
+    @IBOutlet weak var avatar9: UIButton?
+    @IBOutlet weak var avatar10: UIButton?
+    @IBOutlet weak var avatar11: UIButton?
+    @IBOutlet weak var avatar12: UIButton?
+    @IBOutlet weak var avatar13: UIButton?
+    @IBOutlet weak var avatar14: UIButton?
+    @IBOutlet weak var avatar15: UIButton?
+    @IBOutlet weak var avatar16: UIButton?
+    @IBOutlet weak var avatar17: UIButton?
+    @IBOutlet weak var avatar18: UIButton?
+    @IBOutlet weak var avatar19: UIButton?
+    @IBOutlet weak var avatar20: UIButton?
+
+    // MARK: - Methods
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - User Actions
+    @IBAction func startSelected(sender: UIButton)
+    {
+        
+        switch sender
+        {
+        case avatar1:
+        print("Avatar 1 Selected")
+            
+        case avatar2:
+        print("Avatar 2 Selected")
+            
+        case avatar3:
+        print("Avatar 3 Selected")
+            
+        case avatar4:
+        print("Avatar 4 Selected")
+        
+        case avatar5:
+        print("Avatar 5 Selected")
+            
+        case avatar6:
+        print("Avatar 6 Selected")
+            
+        case avatar7:
+        print("Avatar 7 Selected")
+            
+        case avatar8:
+        print("Avatar 8 Selected")
+            
+        case avatar9:
+        print("Avatar 9 Selected")
+            
+        case avatar10:
+        print("Avatar 10 Selected")
+            
+        case avatar11:
+        print("Avatar 11 Selected")
+            
+        case avatar12:
+        print("Avatar 12 Selected")
+            
+        case avatar13:
+        print("Avatar 13 Selected")
+            
+        case avatar14:
+        print("Avatar 14 Selected")
+        
+        case avatar15:
+        print("Avatar 15 Selected")
+            
+        case avatar16:
+        print("Avatar 16 Selected")
+            
+        case avatar17:
+        print("Avatar 17 Selected")
+            
+        case avatar18:
+        print("Avatar 18 Selected")
+            
+        case avatar19:
+        print("Avatar 19 Selected")
+            
+        case avatar20:
+        print("Avatar 20 Selected")
+
+        default:
+            break
+        }
+    }
+    
+    @IBAction func cancel(_ sender: UIButton)
+    {
+        presentingViewController?.dismiss(animated: true)
+    }
+}
